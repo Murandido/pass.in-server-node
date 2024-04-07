c826730c928ba27f9a96374747dfb46095be1366
Murandido <murilo.candidomc97@gmail.com>
2024-04-06 23:41:16 -0300

    new db seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,14 +1,26 @@
 import { prisma } from "../src/lib/prisma";
+import { faker } from "@faker-js/faker";
 
 async function seed() {
   await prisma.event.create({
     data: {
       id: "b3a7c9a1-ec76-4ba3-9e58-df340139c335",
-      title: "Unite Sumit",
-      slug: "unite-sumit",
-      details: "Um evento p/ devs apaixonados(as por código!",
+      title: "Unite Summit",
+      slug: "unite-summit",
+      details: "Um evento p/ devs apaixonados(as) por código!",
       maximumAttendees: 120,
     },
+  });
+
+  await prisma.attendee.createMany({
+    data: Array.from({ length: 120 }).map(() => {
+      return {
+        email: faker.internet.email().toLocaleLowerCase(),
+        eventId: "b3a7c9a1-ec76-4ba3-9e58-df340139c335",
+        name: faker.person.fullName(),
+        createdAt: faker.date.recent({ days: 30 }),
+      };
+    }),
   });
 }
 
